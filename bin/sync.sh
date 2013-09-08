e938f696a26bbd26b0ae363601ca7ee544fbc9c3
Han JU <ju.han.felix@gmail.com>
2013-09-09 00:40:42 +0200

    modified sync script

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
-}
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
-else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-	doIt
-    fi
-fi
+# cd into the repo root
+cd "$(dirname "$0")"
+cd ..
+
+set -e
+
+# update
+git pull
+
 unset doIt
+
+# bash
+rsync -av bash/. ~
 source ~/.bash_profile
+
+# git
+rsync -av git/. ~
+
+# x
+rsync -av x/. ~
+
+# i3wm
+if ! [ -e ~/.i3 ]; then
+    mkdir ~/.i3
+fi
+rsync -av i3/. ~/.i3/
